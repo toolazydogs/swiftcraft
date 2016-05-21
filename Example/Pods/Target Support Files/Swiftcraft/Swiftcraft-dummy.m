c69b48d69f2538a055961556087c319cc1bdd856
Alan D. Cabrera <adc@toolazydogs.com>
2016-05-21 16:01:21 -0700

    Removed Pods directory

--- Example/Pods/Target Support Files/Swiftcraft/Swiftcraft-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Swiftcraft : NSObject
-@end
-@implementation PodsDummy_Swiftcraft
-@end
--- Example/Pods/Target Support Files/Swiftcraft/Swiftcraft-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SwiftcraftVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftcraftVersionString[];
-
